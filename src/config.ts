e98bf4cbb749c3a626c4766dd5ea0ead09e15139
xdfyrj <sumyr@naver.com>
2025-03-23 16:55:28 +0900

    icon

--- src/config.ts
+++ src/config.ts
@@ -31,8 +31,6 @@ export const siteConfig: SiteConfig = {
   favicon: [    // Leave this array empty to use the default favicon
     {
       src: '/favicon/Hiro.png',    // Path of the favicon, relative to the /public directory
-      theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-      sizes: '750x750',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
     }
   ]
 }
